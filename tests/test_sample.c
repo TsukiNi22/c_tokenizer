29e00782063f2ed871590ae8b907c5afa38a8fdc
Tsukini <167094972+TsukiNi22@users.noreply.github.com>
2025-03-04 20:36:04 +0100

    Initial commit

---
+++ tests/test_sample.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** test_my_printf_normal.c
+** File description:
+** Test sample
+*/
+
+#include "debug.h"
+
+/*Test(my_printf, test01_normal_string, .init = redirect)
+{
+    my_print("Hello World!\n");
+    cr_assert_stdout_eq_str("Hello World!\n");
+}*/
